8d49dcc3f2cb6a83d065744c3d10d9883f0a405e
Eric Price <wunderkammer123@gmail.com>
2019-08-19 19:54:55 -0400

    completed NumberGuesser review exercize

--- Review/NumberGuesser.py
+++ Review/NumberGuesser.py
@@ -1,13 +1,30 @@
-# create random number
+import random 
 
-# prompt user to guess number
-# validate input
+# randomNumber = random.randint(1, 10) # create random number
+randomNumber = 3
+guessesRemaining = 5
 
-# track number of guesses made
-# compare guessNumber to randNumber
+while(guessesRemaining >= 1):
+    try:
+        guessNumber = int(input('Guess a number 1 through 10\n')) # prompt user to guess number
+    except ValueError:
+        print('You didn\'t enter an integer. Please, try again.') # validate input
+        continue
+    if(guessNumber < 1 or guessNumber > 10):
+        print('You didn\'t enter an integer between 1 and 10. Please, try again.')
+        continue
+    elif(guessNumber == randomNumber): # compare guessNumber to randNumber
+        print(str(guessNumber) + '! You got it!\nGame Over!') # if correct, return congratulations
+        guessNumber = 0
+        break
+    elif(guessesRemaining > 1):
+        guessesRemaining = guessesRemaining - 1 # track number of guesses made
+        print('Try again.') 
+        continue # else prompt to guess again
+    else:
+        print('Failure. It was ' + str(randomNumber) + '!')
+        break
 
-# if correct, return congratulations
-# else prompt to guess again
 
 
 
