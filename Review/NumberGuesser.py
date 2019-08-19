3a412123147b372ec19225cf273da17f683ed3d6
Eric Price <wunderkammer123@gmail.com>
2019-08-19 17:26:57 -0400

    created Review folder and NumberGuesser project

---
+++ Review/NumberGuesser.py
@@ -0,0 +1,13 @@
+# create random number
+
+# prompt user to guess number
+# validate input
+
+# track number of guesses made
+# compare guessNumber to randNumber
+
+# if correct, return congratulations
+# else prompt to guess again
+
+
+
